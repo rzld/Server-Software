0fce8b0d9c635853cccbf757c6242040d100cd8a
Rizaldi Tri Yanuar <rzldty@gmail.com>
2017-03-26 12:02:22 +0100

    update 2017-03-24 11:58

--- kv/server.c
+++ kv/server.c
@@ -2,21 +2,40 @@
 
 #include "kv.h"
 #include "parser.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 #define NTHREADS = 4;
 #define BACKLOG = 10;
 
 /* Add anything you want here. */
+pthread_mutex_t lock;
+int workerID[100];
 
 /* A worker thread. You should write the code of this function. */
 void* worker(void* p) {
 
 }
 
+void run()
+{
+  pthread_t worker[100], client[100];
+
+  for (int i = 0; i < NTHREADS; i++)
+  {
+    workerID[i] = i;
+    pthread_create(&worker[i], NULL, worker, workerID[i]);
+  }
+}
+
 /* You may add code to the main() function. */
 int main(int argc, char** argv) {
     int cport, dport; /* control and data ports. */
-    
+
 	if (argc < 3) {
         printf("Usage: %s data-port control-port\n", argv[0]);
         exit(1);
@@ -25,6 +44,9 @@ int main(int argc, char** argv) {
         dport = atoi(argv[1]);
 	}
 
+  run();
+
     return 0;
 }
 
+int socket()
