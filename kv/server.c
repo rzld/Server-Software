e2019f0220d10e8f09105ed5730a0073765dc92f
Rizaldi Tri Yanuar <rzldty@gmail.com>
2017-04-01 21:08:46 +0100

    added some codes from references

--- kv/server.c
+++ kv/server.c
@@ -1,5 +1,8 @@
 /* Server program for key-value store. */
 
+//References:
+// https://gist.github.com/batuhangoksu/2b3afe5970b262d54626
+
 #include "kv.h"
 #include "parser.h"
 #include <sys/socket.h>
@@ -20,17 +23,38 @@ pthread_mutex_t lock;
 /* A worker thread. You should write the code of this function. */
 void* worker(void* p) {
   int socket_ = *(int*) p;
+  int inputSize;
   char *message,
        buffer[255];
 
   //welcome message
   message = "Welcome to the KV store.\n";
   write(socket_, message, strlen(message));
+  message = "What do you want to do?\n";
+  write(socket_, message, strlen(message));
 
   //read from client
   enum DATA_CMD cmd;
   char* key;
   char* text;
+  while ((inputSize = recv(socket_, buffer, 255, 0)))
+  {
+    // end of string marker
+    buffer[inputSize] = '\0';
+    // clear message buffer
+    memset(buffer, 0, 255);
+  }
+
+  if (inputSize == 0)
+  {
+    printf("Disconnected.\n");
+    fflush(stdout);
+    close(socket_);
+  }
+  else if (inputSize == -1)
+  {
+    perror("Failed.\n");
+  }
 
   //parse the buffer
   int msg = parse_d(buffer, &cmd, &key, &text);
@@ -39,19 +63,6 @@ void* worker(void* p) {
   if (msg == NULL)
   {
     //close connection
-  }
-}
-
-void run()
-{
-  int newSocket;
-  struct sockaddr_in server, client;
-  pthread_t worker[100], client[100];
-
-  for (int i = 0; i < NTHREADS; i++)
-  {
-    workerID[i] = i;
-    pthread_create(&worker[i], NULL, worker, workerID[i]);
   }
 }
 
@@ -67,7 +78,36 @@ int main(int argc, char** argv) {
         dport = atoi(argv[1]);
 	}
 
-  run();
+  // start writing
+  int socketDesc, clientSocket, c;
+  struct sockaddr_in server, client;
+  pthread_t worker[100], client[100];
+
+  //create socket
+  socketDesc = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketDesc == -1)
+  {
+    printf("Socket creation failed.\n");
+  }
+  printf("Socket created.\n");
+
+  //sockaddr_in structure
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons(dport);
+
+  //bind
+
+  //listen
+
+  //accept and incoming connection
+
+  //thread
+  for (int i = 0; i < NTHREADS; i++)
+  {
+    workerID[i] = i;
+    pthread_create(&worker[i], NULL, worker, workerID[i]);
+  }
 
     return 0;
 }
